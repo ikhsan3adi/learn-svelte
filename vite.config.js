c0b61fb97d69483f401d9c44a3726d40a24665ec
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-15 11:59:33 +0700

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Learn-Svelte/", // change to your repo name
   plugins: [svelte()],
 })
