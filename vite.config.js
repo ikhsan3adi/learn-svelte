9b4b9c4da0d0792d4b7d2fdc4821fbd33ab4fb14
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-17 18:37:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Learn-Svelte/", // change to your repo name
+  base: "/learn-svelte/", // change to your repo name
   plugins: [svelte()],
 })
